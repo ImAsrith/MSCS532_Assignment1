d5146a95cb1cfd97b10767ecfa04a7c9074c8577
ImAsrith <avejandla33913@ucumberlands.edu>
2025-01-12 12:44:59 -0500

    Insertion sort in monotonically decreasing order.

--- insertionsort.py
+++ insertionsort.py
@@ -3,7 +3,7 @@ def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
         j = i - 1
-        while j >= 0 and key < arr[j]:
+        while j >= 0 and key > arr[j]:#Changed < to > for Descending Order
             arr[j + 1] = arr[j]
             j -= 1
         arr[j + 1] = key
@@ -12,3 +12,4 @@ def insertion_sort(arr):
 arr = [12, 11, 13, 5, 6]
 print("Original Array: ", arr)
 print("Sorted Array: ", insertion_sort(arr))
+#Expected Output: [13, 12, 11, 6, 5]
